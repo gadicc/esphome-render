83190e6cf58242cfef091ca561bfb39d70faa173
Gadi Cohen <dragon@wastelands.net>
2024-09-01 11:52:42 +0100

    codemirror: don't rerender on value change

--- src/lib/codemirror.tsx
+++ src/lib/codemirror.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import CodeMirrorOrig, {
+  ReactCodeMirrorProps,
   ReactCodeMirrorRef,
-  useCodeMirror,
+  ViewUpdate,
 } from "@uiw/react-codemirror";
 import { parser as yamlParser } from "@lezer/yaml";
 import { parser as cppParser } from "@lezer/cpp";
@@ -24,26 +25,34 @@ const CodeMirror = React.memo(function CodeMirror(
   props: Parameters<typeof CodeMirrorOrig>[0],
 ) {
   const ref = React.useRef<ReactCodeMirrorRef>(null);
-  const { extensions, value, ...rest } = props;
+  const { extensions, value, onChange } = props;
+  const valueRef = React.useRef<string | undefined>(value);
+
+  const _onChange: ReactCodeMirrorProps["onChange"] = React.useCallback(
+    (value: string, viewUpdate: ViewUpdate) => {
+      valueRef.current = value;
+      onChange?.(value, viewUpdate);
+    },
+    [onChange],
+  );
+
   const combinedExtensions = React.useMemo(
     () => [..._extensions, ...(extensions ?? [])],
     [extensions],
   );
 
-  React.useEffect(() => {
-    console.log(1, value, ref.current, ref.current?.view);
-    if (ref.current?.view) {
-      console.log(2, value);
-      ref.current.view.dispatch({
-        changes: {
-          from: 0,
-          insert: value,
-        },
-      });
-    }
-  }, [ref.current?.view]);
+  const cm = React.useMemo(() => {
+    return (
+      <CodeMirrorOrig
+        ref={ref}
+        extensions={combinedExtensions}
+        onChange={_onChange}
+        value={valueRef.current}
+      />
+    );
+  }, [_onChange, combinedExtensions]);
 
-  return <CodeMirrorOrig ref={ref} extensions={combinedExtensions} {...rest} />;
+  return cm;
 });
 
 export default CodeMirror;
