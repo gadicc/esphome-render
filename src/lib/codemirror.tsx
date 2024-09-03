70740c8118292fb61979e4066f264e67a0506f22
Gadi Cohen <dragon@wastelands.net>
2024-09-03 12:44:11 +0100

    feat(codemirror): more yaml-language support, i.e. folding/indent/etc

--- src/lib/codemirror.tsx
+++ src/lib/codemirror.tsx
@@ -6,20 +6,81 @@ import CodeMirrorOrig, {
 } from "@uiw/react-codemirror";
 import { parser as yamlParser } from "@lezer/yaml";
 import { parser as cppParser } from "@lezer/cpp";
-import { parseMixed } from "@lezer/common";
-import { LRLanguage } from "@codemirror/language";
+import {
+  LRLanguage,
+  delimitedIndent,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  LanguageSupport,
+} from "@codemirror/language";
+import { SyntaxNode, parseMixed } from "@lezer/common";
 
+// See https://codemirror.net/examples/mixed-language/
 const mixedYamlParser = yamlParser.configure({
+  // Mixed support
   wrap: parseMixed((node) => {
     // console.log(node.from, node.name, node);
     return node.name === "BlockLiteralContent" ? { parser: cppParser } : null;
   }),
+
+  // From https://github.com/codemirror/lang-yaml/blob/main/src/yaml.ts
+  props: [
+    indentNodeProp.add({
+      Stream: (cx) => {
+        for (
+          let before = cx.node.resolve(cx.pos, -1) as SyntaxNode | null;
+          before && before.to >= cx.pos;
+          before = before.parent
+        ) {
+          if (before.name == "BlockLiteralContent" && before.from < before.to)
+            return cx.baseIndentFor(before);
+          if (before.name == "BlockLiteral")
+            return cx.baseIndentFor(before) + cx.unit;
+          if (before.name == "BlockSequence" || before.name == "BlockMapping")
+            return cx.column(before.from, 1);
+          if (before.name == "QuotedLiteral") return null;
+          if (before.name == "Literal") {
+            let col = cx.column(before.from, 1);
+            if (col == cx.lineIndent(before.from, 1)) return col; // Start on own line
+            if (before.to > cx.pos) return null;
+          }
+        }
+        return null;
+      },
+      FlowMapping: delimitedIndent({ closing: "}" }),
+      FlowSequence: delimitedIndent({ closing: "]" }),
+    }),
+    foldNodeProp.add({
+      "FlowMapping FlowSequence": foldInside,
+      "BlockSequence Pair BlockLiteral": (node, state) => ({
+        from: state.doc.lineAt(node.from).to,
+        to: node.to,
+      }),
+    }),
+  ],
 });
 
-const mixedYaml = LRLanguage.define({ parser: mixedYamlParser });
+const mixedYamlLanguage = LRLanguage.define({
+  name: "yaml",
+  parser: mixedYamlParser,
+  languageData: {
+    commentTokens: { line: "#" },
+    indentOnInput: /^\s*[\]\}]$/,
+  },
+});
 
+function mixedYaml() {
+  return new LanguageSupport(
+    mixedYamlLanguage,
+    // data.of etc...
+  );
+}
+
+// console.log({ mixedYaml, mixedYamlSupport: mixedYamlSupport(), yaml: yaml() });
 // const extensions = [yaml(), cpp()];
-const _extensions = [mixedYaml];
+
+const _extensions = [mixedYaml()];
 
 const CodeMirror = React.memo(function CodeMirror(
   props: Parameters<typeof CodeMirrorOrig>[0],
