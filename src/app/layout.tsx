e41c039bb656ef8369fb9b111f4f2ec3976c96be
Gadi Cohen <dragon@wastelands.net>
2024-09-03 14:46:42 +0100

    feat(app): add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
+import { Analytics } from "@vercel/analytics/react";
 import theme from "../theme";
 import "./globals.css";
 
@@ -21,7 +22,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <ThemeProvider theme={theme}>
+            {children}
+            <Analytics />
+          </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
